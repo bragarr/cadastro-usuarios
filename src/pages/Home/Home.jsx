f923cd081d71b1e687e7eca719870ab14b4f4b37
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-17 23:25:27 -0400

    style: Primeiros ajustes de layout em Mobile first

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,28 @@ import "./Home.css";
 export function Home() {
     return (
         <section>
-            <h2>Área para formulário</h2>
+            <article className="">
+                <h2 className="form__tittle">Users Registration</h2>
+                <form className="form__registration">
+                    <fieldset className="form__input">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" required className="input__data"/>
+                    </fieldset>
+                    <fieldset className="form__input">
+                        <label htmlFor="email">E-mail</label>
+                        <input type="email" name="email" id="email" required className="input__data"/>
+                    </fieldset>
+                    <fieldset className="form__input">
+                        <label htmlFor="phone">Phone</label>
+                        <input type="tel" name="phone" id="phone" required className="input__data"/>
+                    </fieldset>
+                    <fieldset className="form__input">
+                        <label htmlFor="date__birth">Date of Birth</label>
+                        <input type="date" name="date__birth" id="date__birth" required className="input__data date__birth"/>
+                    </fieldset>
+                    <button type="submit" className="button__save">Save</button>
+                </form>
+            </article>
         </section>
     );
 };
