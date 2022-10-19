0d126f24054203d19359466fb2458fa696791281
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-19 12:43:57 -0400

    style: Mudança de parâmetros para versão mobile/desktop

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,24 +5,40 @@ export function Home() {
         <section className="form__body">
             <h2 className="form__tittle">Users Registration</h2>
                 <form className="form__registration">
-                    <fieldset className="form__input">
-                        <label htmlFor="name" className="label__inputs">Name</label>
-                        <input type="text" name="name" id="name" required className="input__data"/>
-                    </fieldset>
-                    <fieldset className="form__input">
-                        <label htmlFor="email" className="label__inputs">E-mail</label>
-                        <input type="email" name="email" id="email" required className="input__data"/>
-                    </fieldset>
-                    <fieldset className="form__input">
-                        <label htmlFor="phone" className="label__inputs">Phone</label>
-                        <input type="tel" name="phone" id="phone" required className="input__data"/>
-                    </fieldset>
-                    <fieldset className="form__input label__birth">
-                        <label htmlFor="date__birth" className="label__inputs label__birth">Date of Birth</label>
-                        <input type="date" name="date__birth" id="date__birth" required className="input__data date__birth"/>
-                    </fieldset>
-                    <fieldset className="form__button">
-                        <button type="submit" className="button__save">Save</button>
+                    <fieldset className="form__inputs">
+                        <label htmlFor="name" className="label__inputs">
+                            Name
+                            <input type="text" name="name"
+                                id="name"
+                                required
+                                className="input__data"
+                            />
+                        </label>
+                        <label htmlFor="email" className="label__inputs">
+                            E-mail
+                            <input type="email" name="email"
+                                id="email"
+                                required
+                                className="input__data"
+                            />
+                        </label>
+                        <label htmlFor="phone" className="label__inputs">
+                            Phone
+                            <input type="tel" name="phone"
+                                id="phone"
+                                required 
+                                className="input__data"
+                            />
+                        </label>
+                        <label htmlFor="date__birth" className="label__inputs label__birth">
+                            Date of Birth
+                            <input type="date" name="date__birth"
+                                id="date__birth"
+                                required
+                                className="input__data date__birth"
+                            />
+                        </label>
+                        <button className="button__save">Save</button>
                     </fieldset>
                 </form>
         </section>
