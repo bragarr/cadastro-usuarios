a704312ed8dbbea324047db190e7348dd0f0615a
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-10-18 23:24:59 -0400

    style: Ajuste de inputs para cadastro de usuários

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,29 +2,27 @@ import "./Home.css";
 
 export function Home() {
     return (
-        <section>
-            <article className="">
-                <h2 className="form__tittle">Users Registration</h2>
+        <section className="form__body">
+            <h2 className="form__tittle">Users Registration</h2>
                 <form className="form__registration">
                     <fieldset className="form__input">
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="name" className="label__inputs">Name</label>
                         <input type="text" name="name" id="name" required className="input__data"/>
                     </fieldset>
                     <fieldset className="form__input">
-                        <label htmlFor="email">E-mail</label>
+                        <label htmlFor="email" className="label__inputs">E-mail</label>
                         <input type="email" name="email" id="email" required className="input__data"/>
                     </fieldset>
                     <fieldset className="form__input">
-                        <label htmlFor="phone">Phone</label>
+                        <label htmlFor="phone" className="label__inputs">Phone</label>
                         <input type="tel" name="phone" id="phone" required className="input__data"/>
                     </fieldset>
                     <fieldset className="form__input">
-                        <label htmlFor="date__birth">Date of Birth</label>
+                        <label htmlFor="date__birth" className="label__inputs">Date of Birth</label>
                         <input type="date" name="date__birth" id="date__birth" required className="input__data date__birth"/>
                     </fieldset>
                     <button type="submit" className="button__save">Save</button>
                 </form>
-            </article>
         </section>
     );
 };
